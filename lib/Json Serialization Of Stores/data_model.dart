0e1447858486a2040a6c4d91e0610d648489463f
VidishaLuhar <vidisha.l@simformsolutions.com>
2023-06-22 19:08:33 +0530

    in Progress : Json Serialization Of Stores Example

--- lib/Json Serialization Of Stores/data_model.dart
+++ lib/Json Serialization Of Stores/data_model.dart
@@ -1,20 +1,33 @@
-class Post
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'data_model.g.dart';
+@JsonSerializable()
+class DataModel
 {
 
-   late int userId;
-   late int id;
+   late String userId;
+   late String id;
    late String title;
    late String body;
 
-  Post({required this.userId,required this.id, required this.title,required this.body});
+  DataModel({required this.userId,required this.id, required this.title,required this.body});
 
   ///Map to Object
-  Post.fromMap(Map<String,dynamic> map) {
+   DataModel.fromMap(Map<String,dynamic> map) {
     userId = map['userId'];
     id= map['id'];
     title= map['title'];
     body= map['body'];
   }
+
+  @JsonKey(
+    fromJson: DataModel.fromJson(Map<String,dynamic> map) => _$DataFromJson(map)
+  )
+
+  ///Using @JsonSerializable()
+   factory DataModel.fromJson(Map<String,dynamic> map) => _$DataFromJson(map);
+
+   Map<String,dynamic> toJson() => _$DataToJson(this);
 
   ///Object To Map
   Map<String,dynamic> toMap()
--- lib/Json Serialization Of Stores/json_serialization_of_stores_example.dart
+++ lib/Json Serialization Of Stores/json_serialization_of_stores_example.dart
@@ -1,7 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:json_annotation/json_annotation.dart';
+import 'package:mobx_example/Json%20Serialization%20Of%20Stores/data_model.dart';
+
+@JsonSerializable()
 class JsonSerializationOfStoresExample extends StatelessWidget {
-  const JsonSerializationOfStoresExample({super.key});
+  JsonSerializationOfStoresExample({super.key});
+
+  DataModel dataModelObject =
+      DataModel(userId: "001", id: '1', title: 'Flutter', body: 'Hello World');
+  String jsonObject =
+      '{"userId": "001", "id": "1", "title": "Flutter", "body": "Dart"}';
+
+  List<DataModel> listOfDataModelObject = [
+    DataModel(userId: "001", id: '1', title: 'Flutter', body: 'Hello World'),
+    DataModel(userId: "002", id: '2', title: 'Dart', body: 'Hello......')
+  ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -11,6 +27,41 @@ class JsonSerializationOfStoresExample extends StatelessWidget {
         title: const Text(
           "Json Serialization Of Stores Example",
           style: TextStyle(fontSize: 19),
+        ),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ///Json Serialization
+            ElevatedButton(
+                onPressed: () {
+                  Map<String, dynamic> dataModelMap = dataModelObject.toMap();
+                  var json = jsonEncode(dataModelMap);
+                  print(json);
+
+                  ///another way of serializing data to Json
+                  String jsonData=jsonEncode(listOfDataModelObject.map((e) => e.toMap()).toList());
+                  print(jsonData.toString());
+
+                },
+                child: Text(
+                  "Serialize",
+                  style: TextStyle(fontSize: 20),
+                )),
+            SizedBox(width: 25),
+
+            ///Json De-Serialization
+            ElevatedButton(
+              onPressed: () {
+                var decoded = jsonDecode(jsonObject);
+                Map<String, dynamic> dataMap = decoded;
+                DataModel newData = DataModel.fromMap(dataMap);
+                print(newData.body);
+              },
+              child: Text("De-Serialize", style: TextStyle(fontSize: 20)),
+            )
+          ],
         ),
       ),
     );
